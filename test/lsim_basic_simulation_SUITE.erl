893b2b0e9f316d456863b5e1c938eeed7cbc25b9
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-12-29 17:57:43 +0000

    Re-add state based and delta based tests

--- test/lsim_basic_simulation_SUITE.erl
+++ test/lsim_basic_simulation_SUITE.erl
@@ -64,10 +64,10 @@ end_per_testcase(Case, Config) ->
 
 all() ->
     [
-     %state_based_static_test,
-     %state_based_partisan_test,
-     %delta_based_static_test,
-     %delta_based_partisan_test,
+     state_based_static_test,
+     state_based_partisan_test,
+     delta_based_static_test,
+     delta_based_partisan_test,
      join_decompositions_static_test,
      join_decompositions_partisan_test%,
      %pure_op_based_static_test,
