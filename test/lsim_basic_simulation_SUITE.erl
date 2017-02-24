67013818943b9ceea04b0a25d1fe8026ce1bed23
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-02-24 17:47:37 +0000

    Reduce number of nodes

--- test/lsim_basic_simulation_SUITE.erl
+++ test/lsim_basic_simulation_SUITE.erl
@@ -40,7 +40,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("kernel/include/inet.hrl").
 
--define(NODE_NUMBER, 13).
+-define(NODE_NUMBER, 3).
 
 %% ===================================================================
 %% common_test callbacks
@@ -65,11 +65,11 @@ end_per_testcase(Case, Config) ->
 all() ->
     [
      %state_based_static_test%,
-     state_based_hyparview_test,
+     state_based_hyparview_test%,
      %delta_based_static_test,
-     delta_based_hyparview_test,
+     %delta_based_hyparview_test,
      %join_decompositions_static_test,
-     join_decompositions_hyparview_test%,
+     %join_decompositions_hyparview_test%,
      %pure_op_based_static_test,
      %pure_op_based_hyparview_test
     ].
