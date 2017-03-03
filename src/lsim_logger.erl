8a9021922559c9d98e703d355abe93c2384bf30a
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-03 16:35:16 +0000

    Add logger behaviour

---
+++ src/lsim_logger.erl
@@ -0,0 +1,48 @@
+%%
+%% Copyright (c) 2016 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(lsim_logger).
+-author("Vitor Enes Duarte <vitorenesduarte@gmail.com").
+
+-include("lsim.hrl").
+
+-export([start_link/0,
+         put/2]).
+
+%% @doc Start the logger.
+-callback start_link() -> {ok, pid()} | ignore | {error, term()}.
+
+%% @doc Stores some `value()' associated with some `key()'.
+-callback put(key(), value()) -> ok.
+
+-spec start_link() -> {ok, pid()} | ignore | {error, term()}.
+start_link() ->
+    do(start_link, []).
+
+-spec put(key(), value()) -> ok.
+put(Key, Value) ->
+    do(put, [Key, Value]).
+
+%% @private
+do(Function, Args) ->
+    Logger = lsim_config:get(lsim_logger),
+    case Logger of
+        redis ->
+            erlang:apply(lsim_redis_logger, Function, Args)
+    end.
