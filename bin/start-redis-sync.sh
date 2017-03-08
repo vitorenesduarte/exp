529b93fdf1dbda6cfe76bbca928791b7c13920e1
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-08 12:10:02 +0000

    Add script that creates a tunnel to redis pod and downloads all redis pod content

---
+++ bin/start-redis-sync.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+POD_NAME=$(kubectl describe pod redis |
+           grep -E "^Name:" |
+           awk '{print $2}')
+
+PORT=6379
+DIR=$(dirname $0)
+
+kubectl port-forward ${POD_NAME} ${PORT}:${PORT} & PROXY_PID=$!
+
+echo "Port forwarding starting..."
+sleep 3
+
+./$DIR/redis-sync.erl
+
+echo "All files downloaded!"
+
+kill ${PROXY_PID}
+
