217aa2e284cfaa3581210ab9c32a6102ecfa3f0e
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-14 15:57:54 +0000

    Start redis before deploying transmission

--- bin/transmission.sh
+++ bin/transmission.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 declare -A CONFIG
 #ldb_mode;ldb_join_decompositions
 CONFIG[0]="state_based;false"
-CONFIG[1]="delta_based;false"
-CONFIG[2]="delta_based;true"
+#CONFIG[1]="delta_based;false"
+#CONFIG[2]="delta_based;true"
 
 DIR=$(dirname $0)
 SCRIPT=${DIR}/lsim-deploy.sh
@@ -11,6 +13,9 @@ OVERLAY=ring
 SIMULATION=gset
 NODE_NUMBER=3
 NODE_EVENT_NUMBER=50
+
+# start redis
+${DIR}/redis-deploy.sh
 
 for i in "${CONFIG[@]}"
 do
