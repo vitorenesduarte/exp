e34c3f36e8d50a5c2d8c8ace6d9cd0c253c73bbf
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-14 09:33:03 +0000

    Assume values for timestamps not reported

--- priv/evaluation/transmission.py
+++ priv/evaluation/transmission.py
@@ -20,7 +20,8 @@ def ls_grep(dir, filter):
 
 def get_metric_files():
     """
-    Return a dictionary from run (timestamp) to list of metric files.
+    Return a dictionary from run (unique timestamp)
+    to list of metric files.
     """
     d = {}
     dirs = ls(METRIC_DIR)
@@ -75,8 +76,10 @@ def group_by_config(d):
             r[k] = {}
 
         for file in d[dir]:
-            # read metric file and remove start_time to all timestamps
+            # read metric file
             j = read_json(file)
+            # for all types, for all metrics
+            # remove start_time
             for type in j:
                 for m in j[type]:
                     m["timestamp"] -= start_time
@@ -90,27 +93,80 @@ def group_by_config(d):
 
     return r
 
+def get_higher_ts(runs):
+    """
+    Find the higher timestamp of all runs.
+    """
+    higher = 0
+    for run in runs:
+        for metric in run:
+            higher = max(higher, metric["timestamp"])
+
+    return higher
+
+def get_ts_bounds(run):
+    """
+    Find the first and last timestamp of a run.
+    """
+    first = run[0]["timestamp"]
+    last = run[-1]["timestamp"]
+    return (first, last)
+
+def get_last_size(run):
+    """
+    Find the last size of a type.
+    """
+    return run[-1]["size"]
+
+def create_metric(ts, size):
+    """
+    Create metric from timestamp and size.
+    """
+    metric = {}
+    metric["timestamp"] = ts
+    metric["size"] = size
+    return metric
+
 def assume_unknown_values(d):
     """
+    Assume values for timestamps not reported.
     """
-    r = {}
 
     for key in d:
-        print(key)
         for type in d[key]:
-            print(type)
+
+            # find the higher timestamp of all runs for this type
+            higher_ts = get_higher_ts(d[key][type])
+
             for run in d[key][type]:
-                s(run)
-                print("\n\n")
+
+                # find the first and last timestamp of this run
+                (first_ts, last_ts) = get_ts_bounds(run)
+
+                # find the last size of this run
+                last_size = get_last_size(run)
+
+                # create fake values from timestamp 0 to first_ts
+                for i in range(0, first_ts):
+                    metric = create_metric(i, 0)
+                    run.insert(i, metric)
+
+                # create fake values from last_ts to higher_ts
+                for i in range(last_ts + 1, higher_ts + 1):
+                    metric = create_metric(i, last_size)
+                    run.append(metric)
+
+    return d
 
 def s(d):
     print(json.dumps(d, sort_keys=True, indent=2))
 
 def main():
     d = get_metric_files()
-    s(d)
+    #s(d)
     d = group_by_config(d)
-    s(d)
+    #s(d)
     d = assume_unknown_values(d)
+    #s(d)
 
 main()
