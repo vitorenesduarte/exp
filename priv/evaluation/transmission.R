63236f6b69101217a8a59898e46ff4fe441cae2f
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-16 17:15:04 +0000

    Higher resolution graphs

--- priv/evaluation/transmission.R
+++ priv/evaluation/transmission.R
@@ -18,18 +18,20 @@ splot <- function(dir) {
 
   # open device
   #png(filename="r.png", width=700,height=500)
-  png(filename="r.png")
+  png(filename="r.png", width=500, height=500, res=80)
 
   # draw the first line
   first_line <- ls[[1]]
 
-  # get colors
+  # style stuff
   nol = length(ls)
   noc = if(nol >= 3) nol else 3
   colors <- brewer.pal(name="Set1", n=noc)
+  line_width = 2
   line_types = c(1:nol)
   plot_chars <- seq(nol)
 
+  # configure plot
   plot(
     range(maxx),
     range(maxy),
@@ -40,13 +42,13 @@ splot <- function(dir) {
     ylab="Transmission (B)" # y axis label
   )
 
-  # draw the rest of the lines
+  # draw lines
   for(i in 1:length(ls)) {
     lines(
       ls[[i]],
       col=colors[[i]],
       type="b",
-      lwd=1.5,
+      lwd=line_width,
       lty=line_types[[i]],
       pch=plot_chars[[i]]
     )
@@ -56,9 +58,11 @@ splot <- function(dir) {
   legend(
     x=0,
     y=maxy,
-    #cex=0.8, uncomment to reduce legend size
+    # uncomment next line to reduce legend size
+    #cex=0.8,
     legend=get_labels(files),
     col=colors,
+    lwd=line_width,
     lty=line_types,
     pch=plot_chars
   )
