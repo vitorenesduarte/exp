f9d8b1f4cc12a2803b8f47b6302f2a20d3336226
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-25 16:10:55 +0000

    Add possibility to force image build when starting simulation

--- bin/sim.sh
+++ bin/sim.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+DIR=$(dirname $0)
+BRANCH=$(git branch |
+         grep "*" |
+         awk '{print $2}')
+
+if [ "${WHAT}" == "build" ]; then
+  IMAGE=vitorenesduarte/lsim
+  DOCKERFILE=${DIR}/../Dockerfiles/lsim
+
+  # build and push image
+  BRANCH=${BRANCH} \
+    IMAGE=${IMAGE} \
+    DOCKERFILE=${DOCKERFILE} ${DIR}/image.sh
+
+elif [ "${WHAT}" == "run" ]; then
+  IMAGE=vitorenesduarte/lsim
+else
+  # otherwise use image that clones on start
+  IMAGE=vitorenesduarte/lsim-dev
+fi
+
+# start redis
+${DIR}/redis-deploy.sh
+
 declare -A CONFIG
 #ldb_mode;
 #ldb_driven_mode;
@@ -8,23 +32,19 @@ declare -A CONFIG
 CONFIG[0]="state_based;none;false;false"
 #CONFIG[1]="state_based;state_driven;false;false"
 #CONFIG[2]="state_based;digest_driven;false;false"
-#CONFIG[3]="delta_based;none;false;false"
-#CONFIG[4]="delta_based;none;true;false"
-#CONFIG[5]="delta_based;none;false;true"
-#CONFIG[6]="delta_based;none;true;true"
+CONFIG[3]="delta_based;none;false;false"
+CONFIG[4]="delta_based;none;true;false"
+CONFIG[5]="delta_based;none;false;true"
+CONFIG[6]="delta_based;none;true;true"
 
-DIR=$(dirname $0)
-SCRIPT=${DIR}/lsim-deploy.sh
-BRANCH=$(git branch | grep "*" | awk '{print $2}')
 OVERLAY=hyparview
 SIMULATION=gset
-NODE_NUMBER=64
+NODE_NUMBER=80
 NODE_EVENT_NUMBER=50
 
-echo "[$(date +%T)] Starting ${SIMULATION} simulation on branch ${BRANCH}"
-
-# start redis
-${DIR}/redis-deploy.sh
+echo "[$(date +%T)] Starting ${SIMULATION} simulation."
+echo "[$(date +%T)] BRANCH: ${BRANCH}"
+echo "[$(date +%T)] IMAGE: ${IMAGE}"
 
 for i in "${CONFIG[@]}"
 do
@@ -35,6 +55,7 @@ do
   LDB_DGROUP_BACK_PROPAGATION=${R[3]}
 
   BRANCH=${BRANCH} \
+    IMAGE=${IMAGE} \
     LDB_MODE=${LDB_MODE} \
     LDB_DRIVEN_MODE=${LDB_DRIVEN_MODE} \
     LDB_REDUNDANT_DGROUPS=${LDB_REDUNDANT_DGROUPS} \
@@ -42,9 +63,9 @@ do
     OVERLAY=${OVERLAY} \
     SIMULATION=${SIMULATION} \
     NODE_NUMBER=${NODE_NUMBER} \
-    NODE_EVENT_NUMBER=${NODE_EVENT_NUMBER} ${SCRIPT}
+    NODE_EVENT_NUMBER=${NODE_EVENT_NUMBER} ${DIR}/lsim-deploy.sh
 
-  MINUTES=0
+  MINUTES=3
 
   echo "[$(date +%T)] Running ${R[@]}"
   echo "[$(date +%T)] Waiting ${MINUTES} minute(s) before next deploy."
