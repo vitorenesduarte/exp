b9651d54869c8b54d09443d8098caf2471847065
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-04-03 15:13:55 +0100

    Add lsim dash deploy script

---
+++ bin/lsim-dash-deploy.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+RUNNING=$(kubectl get pods | grep lsim-dash)
+
+if [ ! -z "$RUNNING" ]; then
+  echo "[$(date +%T)] lsim-dash already running. Exiting."
+  exit
+fi
+
+# ENV SETUP:
+# Kubernetes server and auth token
+APISERVER=$(kubectl config view |
+            grep "server:" |
+            grep -Eo "https://[0-9\.:]+")
+TOKEN=$(kubectl describe secret |
+        grep "token:" |
+        awk '{print $2}')
+
+# YAML file
+FILE=/tmp/lsim-dash.yaml
+
+cat <<EOF > ${FILE}
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: lsim-dash
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        tag: lsim-dash
+    spec:
+      containers:
+      - name: lsim-dash
+        image: vitorenesduarte/lsim-dash
+        imagePullPolicy: Always
+        env:
+        - name: APISERVER
+          value: "${APISERVER}"
+        - name: TOKEN
+          value: "${TOKEN}"
+EOF
+
+kubectl create -f ${FILE}
