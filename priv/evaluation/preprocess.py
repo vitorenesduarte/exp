7c10ebb5d875b8a08788f9190c981db109a80caa
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-05-21 14:01:03 +0100

    New labels for different ldb eviction round number

--- priv/evaluation/preprocess.py
+++ priv/evaluation/preprocess.py
@@ -64,6 +64,7 @@ def key(config):
         "ldb_mode",
         "ldb_driven_mode",
         "ldb_state_sync_interval",
+        "ldb_eviction_round_number",
         "ldb_redundant_dgroups",
         "ldb_dgroup_back_propagation"
     ]
@@ -72,7 +73,7 @@ def key(config):
     for k in keys:
         l.append(str(config[k]))
 
-    k = "-".join(l)
+    k = "~".join(l)
     return (start_time, k)
 
 def group_by_config(d):
