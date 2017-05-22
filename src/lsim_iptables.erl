5317800700c056392814760750fc05f3dfae9719
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-05-22 14:02:12 +0100

    REJECT OUTPUT chain

--- src/lsim_iptables.erl
+++ src/lsim_iptables.erl
@@ -26,7 +26,7 @@
          reject_ips/1,
          delete_rules/1]).
 
--define(CHAINS, ["INPUT"]).
+-define(CHAINS, ["OUTPUT"]).
 
 %% @doc If number of partitions to be created is greater than 1,
 %%      and not an rsg master, change tcp_keepalive values to:
