66404c8124385116eedb281bd7e9d073d22da43c
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-05-23 11:06:35 +0100

    Add PR link

--- bin/lsim-deploy.sh
+++ bin/lsim-deploy.sh
@@ -142,6 +142,7 @@ spec:
     metadata:
 # Enabling Unsafe Sysctls:
 # - https://kubernetes.io/docs/concepts/cluster-administration/sysctl-cluster/#safe-vs-unsafe-sysctls
+# - https://github.com/kubernetes/kubernetes/pull/26057/files?short_path=8dc23ab#diff-8dc23ab258695ee42154d4d1238c36ef
 #      annotations:
 #        security.alpha.kubernetes.io/unsafe-sysctls: net.ipv4.tcp_keepalive_time=10,net.ipv4.tcp_keepalive_intvl=5,net.ipv4.tcp_keepalive_probes=1
       labels:
