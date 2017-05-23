7bc4926e63172489dbc7f1fe3e52a36f76f85475
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-05-23 10:43:59 +0100

    Don't prompt when deleting the cluster

--- bin/g-cluster.sh
+++ bin/g-cluster.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-NUM_NODES=8
+NUM_NODES=4
 NAME=lsim
 
 if [ "$1" = "start" ]; then
+
+  # create the cluster
   gcloud container clusters \
     create ${NAME} \
     --num-nodes=${NUM_NODES}
 
 elif [ "$1" = "stop" ]; then
-  gcloud container clusters \
+
+  # delete the cluster
+  yes | gcloud container clusters \
     delete ${NAME}
 
 fi
