b743004ea744a27311e2268a8e28b6d20b4b96c0
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-05-30 09:21:43 +0100

    Also draw a bar plot for transmission plots

--- priv/evaluation/transmission.R
+++ priv/evaluation/transmission.R
@@ -1,6 +1,5 @@
 # main function
 main <- function() {
-  source("linesplot.R")
   # draw!
   metrics_dir <- "processed"
   ylabel <- "Transmission (B)"
@@ -13,8 +12,14 @@ main <- function() {
   for(i in 1:length(simulations)) {
     simulation <- simulations[[i]]
     dir <- paste(metrics_dir, simulation, sep="/")
-    output_file <- paste(simulation, "_", key, ".png", sep="")
+
+    source("linesplot.R")
+    output_file <- paste(simulation, "_", key, "_line", ".png", sep="")
     splot(dir, simulation, key, output_file, ylabel, logy)
+
+    source("barplot.R")
+    output_file <- paste(simulation, "_", key, "_bar", ".png", sep="")
+    splot(dir, simulation, key, output_file, ylabel)
   }
 }
 
