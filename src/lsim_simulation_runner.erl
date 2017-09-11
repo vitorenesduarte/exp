eeecd77792bc1f4620766bb87dafa817ecd25dd1
Georges Younes <georges.r.younes@gmail.com>
2017-09-11 16:36:28 +0100

    remove ldb_config from lsim_simulation runner

--- src/lsim_simulation_runner.erl
+++ src/lsim_simulation_runner.erl
@@ -98,7 +98,8 @@ handle_info(event, #state{event_count=Events0,
     Events = Events0 + 1,
     EventFun(Events),
     TotalEvents = TotalEventsFun(),
-    ?LOG("Event ~p | Observed ~p | Node ~p", [Events, TotalEvents, ldb_config:id()]),
+    % ?LOG("Event ~p | Observed ~p | Node ~p", [Events, TotalEvents, ldb_config:id()]),
+    ?LOG("Event ~p | Observed ~p", [Events, TotalEvents]),
 
     case Events == node_event_number() of
         true ->
@@ -113,7 +114,8 @@ handle_info(event, #state{event_count=Events0,
 handle_info(simulation_end, #state{total_events_fun=TotalEventsFun,
                                    check_end_fun=CheckEndFun}=State) ->
     TotalEvents = TotalEventsFun(),
-    ?LOG("Events observed ~p | Node ~p", [TotalEvents, ldb_config:id()]),
+    % ?LOG("Events observed ~p | Node ~p", [TotalEvents, ldb_config:id()]),
+    ?LOG("Events observed ~p", [TotalEvents]),
 
     case CheckEndFun(node_number(), node_event_number()) of
         true ->
