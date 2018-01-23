a302b79a1bea61798e7516867e1f9328568061da
Vitor Enes <vitorenesduarte@gmail.com>
2018-01-23 18:54:40 +0000

    Add clear script

---
+++ bin/clear.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+while [ "${pods}" != "0" ]; do
+  kubectl delete deployment --all
+  sleep 2
+  pods=$(kubectl get pods --show-all 2>&1 | grep -v "No resources found." | wc -l | xargs echo)
+done
