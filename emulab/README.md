8eddba7157eff88ff10323d4dbeaed55dea333f0
Vitor Enes <vitorenesduarte@gmail.com>
2018-09-28 17:25:03 +0100

    Add emulab README

---
+++ emulab/README.md
@@ -0,0 +1,6 @@
+##### How To
+
+- Swap in your experiment in Emulab
+- Copy the list of nodes to __nodes-table__
+- Modify `emulab.config`
+- Run `init.sh`
--- emulab/init.sh
+++ emulab/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-NTYPES=$(awk '{ print $2 }' nodesTable | sort -u | wc -l | xargs echo)
+NODES_FILE=nodes-table
+NTYPES=$(awk '{ print $2 }' ${NODES_FILE} | sort -u | wc -l | xargs echo)
 
 if [ ${NTYPES} -ne 1 ]; then
   echo "Found more than one machine type. Quiting..."
@@ -12,10 +13,10 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 GREEN='\033[0;32m'
 
-Main=$(cat nodesTable | grep main | awk '{print $4}') &&
+Main=$(cat ${NODES_FILE} | grep main | awk '{print $4}') &&
 echo -e "Main done ${GREEN}successfull${NC}" &&
 
-Args=$(tail -n +2 nodesTable | awk '{print $4}' | tr '\n' ' ') &&
+Args=$(tail -n +2 ${NODES_FILE} | awk '{print $4}' | tr '\n' ' ') &&
 echo -e "Args done ${GREEN}successfull${NC}" &&
 
 scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ./init-node.sh ${USER}@"$Main".emulab.net:~/init-node.sh &&
