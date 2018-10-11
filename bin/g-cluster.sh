9d2a96f450c0490254594b64c7a873d6136ab50e
Vitor Enes <vitorenesduarte@gmail.com>
2018-10-11 21:16:51 +0100

    Improve g cluster script

--- bin/g-cluster.sh
+++ bin/g-cluster.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-NUM_NODES=11
+DIR=$(dirname "$0")
+
+NUM_NODES=3
+ZONE=europe-north1-b
 NAME=exp
+MACHINE=n1-standard-8
 MACHINE=n1-standard-1
+VERSION=1.9.7-gke.6
 
 if [ "$1" = "start" ]; then
 
   # create the cluster
   gcloud container clusters \
     create ${NAME} \
+    --zone=${ZONE} \
     --num-nodes=${NUM_NODES} \
-    --machine-type=${MACHINE}
+    --machine-type=${MACHINE} \
+    --cluster-version ${VERSION} \
+    --preemptible
 
 elif [ "$1" = "stop" ]; then
 
