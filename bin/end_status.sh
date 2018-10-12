f4b9d075718f2a27306749e52ba26c9c6e5b395b
Vitor Enes <vitorenesduarte@gmail.com>
2018-10-12 20:37:16 +0100

    subset of status

--- bin/end_status.sh
+++ bin/end_status.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+MAX_LINES=50
+
 show_pod() {
     pod=$1
-    status=$(kubectl logs ${pod} |
+    status=$(kubectl logs ${pod} --since=10s |
         grep -Eo "Events observed [0-9]+" |
         tail -n 1 |
         awk '{ print $3 }')
@@ -12,6 +14,8 @@ show_pod() {
 pods=$(kubectl get pods --no-headers |
     grep Running |
     grep exp- |
+    sort -R |
+    head -n ${MAX_LINES} |
     awk '{ print $1 }')
 
 for pod in ${pods[@]}; do
--- bin/status.sh
+++ bin/status.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+MAX_LINES=20
+
 show_pod() {
     pod=$1
-    status=$(kubectl logs ${pod} |
+    status=$(kubectl logs ${pod} --since=10s |
         grep -Eo "Event [0-9]+ \| Observed [0-9]+" |
         tail -n 1 |
         awk '{ print $2" of " $5 }')
@@ -12,6 +14,8 @@ show_pod() {
 pods=$(kubectl get pods --no-headers |
     grep Running |
     grep exp- |
+    sort -R |
+    head -n ${MAX_LINES} |
     awk '{ print $1 }')
 
 for pod in ${pods[@]}; do
