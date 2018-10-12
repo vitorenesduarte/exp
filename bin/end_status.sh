7574c7cc930400ab9b8b803867e1ca74ed063bb5
Vitor Enes <vitorenesduarte@gmail.com>
2018-10-12 10:43:25 +0100

    Add end status script

--- bin/end_status.sh
+++ bin/end_status.sh
@@ -3,9 +3,9 @@
 show_pod() {
     pod=$1
     status=$(kubectl logs ${pod} |
-        grep -Eo "Event [0-9]+ \| Observed [0-9]+" |
+        grep -Eo "Events observed [0-9]+" |
         tail -n 1 |
-        awk '{ print $2" of " $5 }')
+        awk '{ print $3 }')
     echo "${pod}: ${status}"
 }
 
