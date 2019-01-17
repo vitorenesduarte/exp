f0ec224e67a5558f9e2b9ed0c9aef2bc14f0c919
Vitor Enes <vitorenesduarte@gmail.com>
2019-01-17 08:53:59 +0000

    Improve retwis plot script

--- evaluation/icde19/retwis.R
+++ evaluation/icde19/retwis.R
@@ -1,13 +1,17 @@
 source("util.R")
 source("generic.R")
 
+NLINES <- 2
+EXP_START <- 0
+EXP_END <- 50
+
 get_lines <- function(clusters, key, file_index, first_entry, last_entry) {
   any_non_zero <- 0.1
   map(clusters, function(cluster) {
     files <- system(cluster, intern=TRUE)
 
     # skip if no file
-    if(length(files) == 2) {
+    if(length(files) == NLINES) {
       lines <- json(c(files[file_index]))[[key]]
       entries <- lines[first_entry:last_entry]
       sum(entries) / length(entries) / 1000
@@ -18,8 +22,9 @@ get_lines <- function(clusters, key, file_index, first_entry, last_entry) {
 
 get_all_lines <- function(clusters, key, first_entry, last_entry) {
   lines_y <- list()
-  lines_y[[1]] <- get_lines(clusters, key, 1, first_entry, last_entry)
-  lines_y[[2]] <- get_lines(clusters, key, 2, first_entry, last_entry)
+  for(i in seq(1, NLINES)) {
+    lines_y[[i]] <- get_lines(clusters, key, i, first_entry, last_entry)
+  }
   lines_y
 }
 
@@ -28,11 +33,11 @@ main <- function() {
   output_file <- "retwis.png"
 
   clusters <- c(
-    "ls -d processed/* | grep ~50~0~retwis",
-    "ls -d processed/* | grep ~75~0~retwis",
-    "ls -d processed/* | grep ~100~0~retwis",
-    "ls -d processed/* | grep ~125~0~retwis",
-    "ls -d processed/* | grep ~150~0~retwis"
+    "ls -d processed/* | grep ~50~0~retwis | grep -v op_based | grep -v scuttlebutt",
+    "ls -d processed/* | grep ~75~0~retwis | grep -v op_based | grep -v scuttlebutt",
+    "ls -d processed/* | grep ~100~0~retwis | grep -v op_based | grep -v scuttlebutt",
+    "ls -d processed/* | grep ~125~0~retwis | grep -v op_based | grep -v scuttlebutt",
+    "ls -d processed/* | grep ~150~0~retwis | grep -v op_based | grep -v scuttlebutt"
   )
   labels <- c(
     "Delta-based",
@@ -57,45 +62,56 @@ main <- function() {
     "springgreen4",
     "gray22"
   )
+  pch <- c(3,6)
+
+  # assert there's enough info
+  stopifnot(length(labels) == NLINES)
+  stopifnot(length(colors) == NLINES)
+  stopifnot(length(pch) == NLINES)
 
   coefs <- c(0.5, 0.75, 1, 1.25, 1.5)
   lines_x <- list()
-  lines_x[[1]] <- coefs
-  lines_x[[2]] <- coefs
+  for(i in seq(1, NLINES)) {
+    lines_x[[i]] <- coefs
+  }
   x_lab <- "Zipf coefficients"
 
   # first plot
   key <- "transmission_term_size"
   y_lab <- "Transmission (GB/s)"
-  lines_y_1 <- get_all_lines(clusters, key, 0, 25)
-  lines_y_2 <- get_all_lines(clusters, key, 25, 50)
+  lines_y_1 <- get_all_lines(clusters, key, EXP_START, EXP_END / 2)
+  lines_y_2 <- get_all_lines(clusters, key, EXP_END / 2, EXP_END)
 
   plot_lines_retwis(lines_x, lines_y_1, colors,
                     y_lab=y_lab,
                     y_max=2,
-                    lwd=2)
+                    lwd=2,
+                    pch=pch)
   title("0%-50%", cex.main=1.3)
 
   plot_lines_retwis(lines_x, lines_y_2, colors,
                     y_max=2,
-                    lwd=2)
+                    lwd=2,
+                    pch=pch)
   title("50%-100%", cex.main=1.3)
   print(lines_y_2)
 
   # second plot
   key <- "memory_term_size"
   y_lab <- "Avg. Memory (GB)"
-  lines_y_1 <- get_all_lines(clusters, key, 0, 25)
-  lines_y_2 <- get_all_lines(clusters, key, 25, 50)
+  lines_y_1 <- get_all_lines(clusters, key, EXP_START, EXP_END / 2)
+  lines_y_2 <- get_all_lines(clusters, key, EXP_END / 2, EXP_END)
 
   plot_lines_retwis(lines_x, lines_y_1, colors,
                     y_lab=y_lab,
                     y_max=2,
-                    lwd=2)
+                    lwd=2,
+                    pch=pch)
 
   plot_lines_retwis(lines_x, lines_y_2, colors,
                     y_max=2,
-                    lwd=2)
+                    lwd=2,
+                    pch=pch)
   print(lines_y_2)
 
   x_axis_label(x_lab)
@@ -112,7 +128,7 @@ main <- function() {
     inset=-.25,
     cex=1,
     legend=labels,
-    pch=c(3,6),
+    pch=pch,
     col=colors,
     ncol=2,
     box.col=NA # remove box
